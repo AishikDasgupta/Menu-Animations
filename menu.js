caddc37995f9248e6f719a9422f648963ce25cd9
Aishik Dasgupta <83045121+AishikDasgupta@users.noreply.github.com>
2024-09-12 23:17:33 +0530

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,10 @@
+/* THE JAVASCIRPT IS EMBEDDED IN THE HTML FILE DUE TO COMPATIBILITY ISSUES WITH MY BROWSER*/
+/* const menu = document.getElementById("menu");
+
+Array.from(document.getElementsByClassName("menu-item"))
+  .forEach((item, index) => {
+    item.onmouseover = () => {
+      menu.dataset.activeIndex = index;
+    }
+  });
+*/
